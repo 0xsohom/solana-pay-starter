8ae1a96d8ef4bde58af0bcb2150a8449a3af9fbc
Sohom Pal <sohom@Sohoms-MacBook-Air.local>
2022-09-27 23:14:02 +0530

    something

---
+++ pages/api/createTransaction.js
@@ -0,0 +1,11 @@
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  clusterApiUrl,
+  Connection,
+  PublicKey,
+  Transaction,
+  SystemProgram,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js";
+import BigNumber from "bignumber.js";
+import products from "./products.json";
